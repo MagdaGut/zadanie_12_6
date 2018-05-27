ef261cee81c226a83522d4dfeb4cb08ca1c70c80
Michał Gacek <gacek.michael@outlook.com>
2018-05-27 10:14:18 +0200

    Fixed styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
 // scripts.js
+$(function() {
+    var url = 'https://restcountries.eu/rest/v1/name/';
+    var $countriesList = $('#countries');
+    
+    $('#search').click(searchCountries);
 
-var url = 'https://restcountries.eu/rest/v1/name/';
-    $(function(){
-        var $countriesList = $('#countries');
-            $('#search').click(searchCountries);
-
-        function searchCountries() {
-            var countryName = $('#country-name').val();
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        
         if (!countryName.length) {
-                countryName = 'Poland';
-                $('#country-name').val('Poland');
-            }   
+            countryName = 'Poland';
+            $('#country-name').val('Poland');
+        }   
+
         $.ajax({
             url: url + countryName,
             method: 'GET',
             success: showCountriesList
         });                                 
-        }
+    }
 
-        function showCountriesList(resp){
-            $countriesList.empty();
-            resp.forEach(function(item){
-                $('<li>').text(item.name+" ("+item.capital+")").appendTo($countriesList);
-            });
-        }
-
-    })
+    function showCountriesList(resp){
+        $countriesList.empty();
+        resp.forEach(function(item){
+            $('<li>').text(item.name+" ("+item.capital+")").appendTo($countriesList);
+        });
+    }
+});
